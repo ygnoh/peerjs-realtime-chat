f3daeae6ba080c52bfc008a02e5d8e78bfc22f8e
yonggoo-noh <yonggoo.noh@navercorp.com>
2021-07-24 01:06:40 +0900

    Show my id

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
+import Peer from "peerjs";
 
 class App extends React.PureComponent {
+    state = {
+        id: undefined
+    };
+
+    componentDidMount() {
+        this._peer = new Peer();
+
+        this._peer.on("open", id => {
+            this.setState({id});
+        });
+    }
+
     render() {
+        const {id} = this.state;
+
         return (
-            <p>Hello world</p>
+            <div>
+                <p>Connect with me: {id}</p>
+            </div>
         );
     }
 }
